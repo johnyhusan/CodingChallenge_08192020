c1d051698e15c0a42313b895e52befb351ec5184
Johny Husan <johnyhusan@gmail.com>
2020-08-20 02:03:09 +0500

    JS challenge

--- main.js
+++ main.js
@@ -1,17 +1,33 @@
 /*****************************************************************************************
-* Part 2
-****************************************************************************************/
+ * Part 2
+ ****************************************************************************************/
 var employees = [
-        {first: "Amanda", last: "Byron", group: "Sales"},
-        {first: "Ye", last: "Xia", group: "Receiving", nameOrder: "reverse"},
-        {first: "Miltiades", last: "Crescens", group: "Sales"},
-        /*...don't foget to account for other entries of the same form, but with different group names.....*/
-    ];
+  { first: "Amanda", last: "Byron", group: "Sales" },
+  { first: "Ye", last: "Xia", group: "Receiving", nameOrder: "reverse" },
+  { first: "Miltiades", last: "Crescens", group: "Sales" },
+  /*...don't foget to account for other entries of the same form, but with different group names.....*/
+];
 
 // Part 2 Answer Here
+function makeGroup(employeeList) {
+  return employeeList.reduce(function (obj, employee) {
+    let name =
+      employee.nameOrder === "reverse"
+        ? employee.last + " " + employee.first
+        : employee.first + " " + employee.last;
+
+    if (obj[employee.group]) {
+      obj[employee.group].push({ name });
+    } else {
+      obj[employee.group] = [{ name }];
+    }
+
+    return obj;
+  }, {});
+}
 
 /*****************************************************************************************
-* Bonus
-****************************************************************************************/
+ * Bonus
+ ****************************************************************************************/
 
 // Bonus Anwser Here
